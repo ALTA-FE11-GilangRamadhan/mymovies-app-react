0a0a815860b98336df2ce03081ff2e8ee5889c42
Gilang Ramadhan <gilangr1440@gmail.com>
2023-01-05 09:03:28 +0700

    Release 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import DetailMovie from "./pages/DetailMovie";
+import Favorite from "./pages/Favorite";
+import App from "./pages";
+import Index from "./pages/index";
+import "./styles/index.css";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3/movie/";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Index />
+  </React.StrictMode>
+);
